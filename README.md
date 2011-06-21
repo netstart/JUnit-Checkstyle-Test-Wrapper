dc9dc2110a83516c783f5403206b77717c774e90
Christoffer Pettersson <christoffer@christoffer.me>
2011-06-21 21:55:48 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,77 @@
+Wraps the static code analyze tool Checkstyle into a simple JUnit test. 
+
+![alt text](https://github.com/corgrath/JUnit-Checkstyle-Test-Wrapper/raw/master/doc/wiki/results.png "The JUnit results in Eclipse")
+
+
+
+Download
+========================================
+Download the latest version here:
+https://github.com/corgrath/JUnit-Checkstyle-Test-Wrapper/downloads/
+
+
+How to use it in an Eclipse project
+========================================
+
+Download JUnit Checkstyle Test wrapper
+----------------------------------------
+
+Download the latest version of JUnit Checkstyle Test Wrapper and add it to the project. 
+
+![alt text](https://github.com/corgrath/JUnit-Checkstyle-Test-Wrapper/raw/master/doc/wiki/install_checkstyle_wrapper.png "Download JUnit-Checkstyle-Test-Wrapper and add it to Eclipse")
+
+Download Checkstyle
+----------------------------------------
+
+Download the latest version of Checkstyle and add it to the project.
+
+Download Checkstyle from: https://sourceforge.net/projects/checkstyle/files/ 
+
+![alt text](https://github.com/corgrath/JUnit-Checkstyle-Test-Wrapper/raw/master/doc/wiki/install_checkstyle.png "Download Checkstyle and add it to Eclipse")
+
+Create a JUnit test case
+----------------------------------------
+
+Write a JUnit test case using the wrapper.
+
+	package src;
+	
+	import me.christoffer.junitcheckstylewrapper.JunitCheckstyleTestWrapper;
+	
+	import org.junit.Test;
+	
+	public class TestCheckstyle {
+	
+	        @Test
+	        public void testSrc() throws Exception {
+	
+	                JunitCheckstyleTestWrapper.run(this, "./src/", "checkstyle.xml");
+	
+	        }
+	
+	}
+		
+![alt text](https://github.com/corgrath/JUnit-Checkstyle-Test-Wrapper/raw/master/doc/wiki/eclipse_testcheckstyle.png "Create the JUnit test in Eclipse")
+
+Create a Checkstyle rule set file
+----------------------------------------
+
+Create a Checkstyle rule set file and add it in the same folder as the test.
+
+Guide: http://checkstyle.sourceforge.net/availablechecks.html
+
+Or download our sample file: https://github.com/corgrath/JUnit-Checkstyle-Test-Wrapper/raw/master/doc/wiki/checkstyle.xml
+
+![alt text](https://github.com/corgrath/JUnit-Checkstyle-Test-Wrapper/raw/master/doc/wiki/eclipse_checkstyle_xml.png "Create the rule files for Checkstyle")
+
+Run and view the results
+----------------------------------------
+
+![alt text](https://github.com/corgrath/JUnit-Checkstyle-Test-Wrapper/raw/master/doc/wiki/results.png "The JUnit results in Eclipse")
+
+License
+========================================
+
+JUnit Checkstyle Test Wrapper - Copyright 2011 Christoffer Pettersson, christoffer@christoffer.me
+
+Licensed under the Apache License, Version 2.0
---
+++ src/me/christoffer/junitcheckstyletestwrapper/JUnitCheckstyleTestWrapper.java
@@ -0,0 +1,145 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ */
+
+package me.christoffer.junitcheckstyletestwrapper;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.xml.sax.InputSource;
+
+import com.puppycrawl.tools.checkstyle.Checker;
+import com.puppycrawl.tools.checkstyle.ConfigurationLoader;
+import com.puppycrawl.tools.checkstyle.DefaultLogger;
+import com.puppycrawl.tools.checkstyle.PropertiesExpander;
+import com.puppycrawl.tools.checkstyle.api.AuditListener;
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import com.puppycrawl.tools.checkstyle.api.Configuration;
+
+/**
+ * Contains the logic for the JUnit Checkstyle test wrapper
+ *
+ * @author Christoffer Pettersson, christoffer@christoffer.me
+ */
+
+public abstract class JUnitCheckstyleTestWrapper {
+
+	/**
+	 * Runs the JUnit Checkstyle test wrapper on a specific folder based on a given rule set file in the same folder
+	 * 
+	 * @param testClassInstance The JUnit test instance
+	 * @param folderToCheck The folder that should be tested
+	 * @param ruleFileName Name of the Checkstyle rule set file that should be located in the same folder
+	 * @throws FileNotFoundException If any errors occur
+	 * @throws CheckstyleException  If any error occur
+	 */
+
+	public static void run(final Object testClassInstance, final String folderToCheck, final String ruleFileName) throws FileNotFoundException, CheckstyleException {
+
+		File fileFolderToCheck = new File(folderToCheck);
+
+		System.out.println("Starting Checkstyle on folder '" + fileFolderToCheck.getAbsolutePath() + "'..");
+
+		/*
+		 * Validations
+		 */
+
+		if (!fileFolderToCheck.exists()) {
+			throw new FileNotFoundException("The folder to check '" + fileFolderToCheck.getAbsolutePath() + "' does not exist.");
+		}
+
+		if (!fileFolderToCheck.isDirectory()) {
+			throw new FileNotFoundException("The folder to check '" + fileFolderToCheck.getAbsolutePath() + "' is not a directory.");
+		}
+
+		if (testClassInstance.getClass().getResource(ruleFileName) == null) {
+			throw new FileNotFoundException("The rule set file '" + ruleFileName + "' does not exist in the same folder as '" + testClassInstance.getClass().getSimpleName() + "'.");
+		}
+
+		/*
+		 * Files
+		 */
+
+		List<File> files = new ArrayList<File>();
+		listFiles(files, fileFolderToCheck, "java");
+		System.out.println("Found " + files.size() + " Java source files.");
+
+		if (files.isEmpty()) {
+			Assert.fail("Found no Java source files. Configuration error?");
+		}
+
+		/*
+		 * Listener
+		 */
+
+		ByteArrayOutputStream sos = new ByteArrayOutputStream();
+		AuditListener listener = new DefaultLogger(sos, false);
+
+		/*
+		 * Configuration
+		 */
+
+		InputSource inputSource = new InputSource(testClassInstance.getClass().getResourceAsStream(ruleFileName));
+		Configuration configuration = ConfigurationLoader.loadConfiguration(inputSource, new PropertiesExpander(System.getProperties()), false);
+
+		/*
+		 * Create checker
+		 */
+
+		Checker checker = new Checker();
+		checker.setModuleClassLoader(Checker.class.getClassLoader());
+		checker.configure(configuration);
+		checker.addListener(listener);
+
+		/*
+		 * Process
+		 */
+
+		int errors = checker.process(files);
+		System.out.println("Found " + errors + " check style errors.");
+		System.out.println(sos.toString());
+		Assert.assertTrue(errors + " check style errors found. " + sos.toString(), errors == 0);
+
+		/*
+		 * Clean up
+		 */
+
+		checker.destroy();
+
+	}
+
+	/**
+	 * Lists all files in a given folder
+	 */
+
+	private static void listFiles(final List<File> files, final File folder, final String extension) {
+		if (folder.canRead()) {
+			if (folder.isDirectory()) {
+				for (File f : folder.listFiles()) {
+					listFiles(files, f, extension);
+				}
+			} else if (folder.toString().endsWith("." + extension)) {
+				files.add(folder);
+			}
+		}
+	}
+
+}
